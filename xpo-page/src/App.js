3d1c2b9f2e5d3493d4beba6c1896664eb257e2c1
Sayak Ghosh <sayakmemari@gmail.com>
2022-06-01 21:20:17 +0530

    intial-commit

---
+++ xpo-page/src/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './App.css';
+import NavbarComp from './Components/NavbarComp';
+class App extends Component {
+  render() {
+    return(
+      <>
+        <NavbarComp />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ xpo-page/src/Components/NavbarComp.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+import "./NavbarComp.css";
+import { Nav, NavDropdown, Button } from "react-bootstrap";
+import { BsFillBellFill } from "react-icons/bs";
+import { AiFillDollarCircle } from "react-icons/ai";
+import {AiFillCaretDown} from "react-icons/ai";
+class NavbarComp extends React.Component {
+  render() {
+    return (
+      <>
+        <nav className="main-nav">
+          {/* Logo */}
+          <div className="logo">
+            <h2>Solera</h2>
+          </div>
+
+          {/* Middle  Menu */}
+          <div className="menu-link">
+            <ul>
+              <li>
+                <Nav.Link href="#campaigns">Campaigns</Nav.Link>
+              </li>
+              <li>
+                <NavDropdown title="Audience" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </li>
+              <li>
+                <NavDropdown title="Creative" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </li>
+              <li>
+                <NavDropdown title="Catalogs" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </li>
+              <li>
+                <NavDropdown title="Utility" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </li>
+              <li>
+                <NavDropdown title="Reports" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </li>
+              <li>
+                <NavDropdown title="Pixel" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </li>
+              <li>
+                <NavDropdown title="Settings" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </li>
+            </ul>
+          </div>
+
+          {/* Right Most Menu */}
+          <div className="right-container">
+            <div className="bell-icon">
+              <BsFillBellFill size={30} />
+            </div>
+            <div className="usd-icon">
+                <AiFillDollarCircle size={30} />
+                <span>USD</span>
+            </div>
+            <div className="extreme-container">
+              <p className="name-salutation">Hi, Hugo Solis</p>
+              <div className="bottom-container">
+                <p className="identity">Identifix</p>
+                <div className="button">
+                  <button class="button-18" role="button">Account</button>
+                </div>
+                <AiFillCaretDown />
+              </div>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
+export default NavbarComp;
