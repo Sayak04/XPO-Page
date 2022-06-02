9cfc7af782e2da18f33731d594f3721491e4ebea
Sayak Ghosh <sayakmemari@gmail.com>
2022-06-03 00:56:50 +0530

    campaign6 started

--- xpo-page/src/Components/Campaign6.jsx
+++ xpo-page/src/Components/Campaign6.jsx
@@ -1,23 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Campaign6 extends Component {
   render() {
     return (
-        <>
-            <div className="campaign-container">
-                <div className="symbols">
-                    <div className="circle">
-                        <p> 6 </p>
-                    </div>
-                    <div className="line"></div>
-                </div>
-                <div className="campaign-box">
-                    <h2>
-                        Targeting
-                    </h2>
-                </div>
+      <>
+        <div className="campaign-container">
+          <div className="symbols">
+            <div className="circle">
+              <p> 6 </p>
             </div>
-        </>
-    )
+            <div className="line"></div>
+          </div>
+          <div className="campaign-box">
+            <h2>Targeting</h2>
+            <div className="top-container">
+              <div className="first-container first-container-campaign6">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
