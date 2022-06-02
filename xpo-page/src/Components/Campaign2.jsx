ddb22a14f3017fe898dad2634b2abdc0b46f5d92
Sayak Ghosh <sayakmemari@gmail.com>
2022-06-02 19:12:53 +0530

    campaign2

--- xpo-page/src/Components/Campaign2.jsx
+++ xpo-page/src/Components/Campaign2.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import './Campaign1.css';
-import './Campaign2.css';
+import "./Campaign1.css";
+import "./Campaign2.css";
+import { TbWorldUpload } from "react-icons/tb";
+import { BsFacebook, BsLinkedin } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
 
 export default class Campaign2 extends Component {
   render() {
@@ -14,14 +17,34 @@ export default class Campaign2 extends Component {
             <div className="line"></div>
           </div>
           <div className="campaign-box">
-            <h2>
-                Select One or More Channels
-            </h2>
+            <h2>Select One or More Channels</h2>
             <div className="inside-container">
-
+              <div className="four-containers">
+                <div className="programmatic">
+                  <input type="checkbox" className="checkbox"/>
+                  <TbWorldUpload className="icon" size={45} color={'rgb(136, 136, 4)'}/>
+                  <p> Programmatic </p>
+                </div>
+                <div className="facebook">
+                  <input type="checkbox" className="checkbox"/>
+                  <BsFacebook className="icon" size={45} color={'rgb(66, 103, 178)'}/>
+                  <p> Facebook </p>
+                </div>
+                <div className="search">
+                  <input type="checkbox" className="checkbox"/>
+                  <FaSearch className="icon" size={45} color={'brown'}/>
+                  <p> Search </p>
+                </div>
+                <div className="linkedin">
+                  <input type="checkbox" className="checkbox"/>
+                  <BsLinkedin className="icon" size={45} color={'rgb(66, 103, 178)'}/>
+                  <p> LinkedIn </p>
+                </div>
+              </div>
             </div>
             <div className="message-container">
-                * Programmatic channels include Connected Tvs, OTT, Video, Audio, Display, Native and Email. 
+              * Programmatic channels include Connected Tvs, OTT, Video, Audio,
+              Display, Native and Email.
             </div>
           </div>
         </div>
