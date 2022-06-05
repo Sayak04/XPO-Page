99034499a2109151b9a59bd045d970421ddc6f4d
Sayak Ghosh <sayakmemari@gmail.com>
2022-06-06 00:03:23 +0530

    Updates

---
+++ precache-manifest.c68560a9c59373c3f87d7085f16a01d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dd26818a177e44d011d",
+    "url": "/XPO-Page/static/css/main.6c6cd855.chunk.css"
+  },
+  {
+    "revision": "7dd26818a177e44d011d",
+    "url": "/XPO-Page/static/js/main.7dd26818.chunk.js"
+  },
+  {
+    "revision": "4567e6b4a7bd4ab0b748",
+    "url": "/XPO-Page/static/js/1.4567e6b4.chunk.js"
+  },
+  {
+    "revision": "11aafe1a1e7b1d64470d",
+    "url": "/XPO-Page/static/js/2.11aafe1a.chunk.js"
+  },
+  {
+    "revision": "9cb147b68950af6416dd",
+    "url": "/XPO-Page/static/js/runtime~main.9cb147b6.js"
+  },
+  {
+    "revision": "1c8d2bc788e945176a36c6eeb40b4326",
+    "url": "/XPO-Page/static/media/india-google-map.1c8d2bc7.jpg"
+  },
+  {
+    "revision": "4da204628a39535af4ea2b24e5231b26",
+    "url": "/XPO-Page/index.html"
+  }
+];
